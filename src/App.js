868895fff09ce5dbfa1db89195746853244f274f
RajaeKacemi <rajaekacemi@gmail.com>
2023-10-09 16:44:58 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+export default function App() {
+const name = 'Rajaa';
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const myContainer = (
+  
+    <>
+  <h2>Dear Stephen</h2>
+<br/>
+<p>Hi, my name is {name}, and I would like to introduce myself to you. I am applying for the position of React developer.</p>
+<br/>
+<p>I’ve attached my CV to this email.</p>
+<p>You can find details of my previous roles, experience, and achievements.</p>
+<p>I’m particularly proud of :</p>
+<ul id = 'my-achievements'>
+    <li className = 'ln-achievement'>achievement 1</li>
+    <li className = 'ln-achievement'>achievement 2</li>
+    <li className = 'ln-achievement'>achievement 3</li>
+</ul>
+<p>I’m excited about this opportunity and would love to know more about the process, including timelines for you to decide on interviews.</p>
+<br/>
+<span>Best regards,</span>
+<span>React</span>
+<br/>
+<b>{day}/{month}/{year}</b>
+<br></br>
+<b>{name}</b>
+  </>
   );
+  
+  return myContainer;
 }
-
-export default App;
